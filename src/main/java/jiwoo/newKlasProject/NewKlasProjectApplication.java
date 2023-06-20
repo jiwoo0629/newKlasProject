943130005eac7d165b1dc7b7352e4ff99c00daf0
jiwoo0629 <wldn990629@gmail.com>
2023-06-20 17:22:12 +0900

    initial commit

---
+++ src/main/java/jiwoo/newKlasProject/NewKlasProjectApplication.java
@@ -0,0 +1,14 @@
+package jiwoo.newKlasProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude= SecurityAutoConfiguration.class)
+public class NewKlasProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewKlasProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jiwoo/newKlasProject/NewKlasProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jiwoo.newKlasProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewKlasProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
